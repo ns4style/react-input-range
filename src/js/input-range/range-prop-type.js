6c94c9adee60a1802ae359415c716a1f07d55ca7
Artem <ns4style@mail.ru>
2018-12-06 16:44:08 +0700

    remove error

--- src/js/input-range/range-prop-type.js
+++ src/js/input-range/range-prop-type.js
@@ -6,13 +6,14 @@ import { isNumber } from '../utils';
  * @return {?Error} Return Error if validation fails
  */
 export default function rangePropType(props) {
-  const { maxValue, minValue } = props;
+  const {maxValue, minValue, allowSameValues} = props;
 
   if (!isNumber(minValue) || !isNumber(maxValue)) {
     return new Error('"minValue" and "maxValue" must be a number');
   }
-
-  if (minValue >= maxValue) {
-    return new Error('"minValue" must be smaller than "maxValue"');
+  if (!allowSameValues) {
+    if (minValue >= maxValue) {
+      return new Error('"minValue" must be smaller than "maxValue"');
+    }
   }
 }
